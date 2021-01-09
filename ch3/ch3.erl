e79e1b9c736826add3df8a382f7411253f1f7b17
Vladislav Kislitsyn <v_kislitsyn@wargaming.net>
2021-01-09 14:13:28 +0300

    ch3 filter+reverse+concatenate tasks done

--- ch3/ch3.erl
+++ ch3/ch3.erl
@@ -1,5 +1,5 @@
 -module(ch3).
--export([sum/1, sumRange/2, reverse_create/1, create/1, print/1]).
+-export([sum/1, sumRange/2, reverse_create/1, create/1, print/1, filter/2,reverse/1, concatenate/1]).
 
 % 3-1 Write sum function
 %---
@@ -29,3 +29,33 @@ print(Idx, Num) when Idx =< Num ->
 	io:format("Number:~p~n", [Idx]),
 	print(Idx + 1, Num);
 print(_, _) -> {ok}.
+
+% 3-5 Lists transformation
+%---
+filter([], _) -> [];
+filter([Item|Rest], Num) when Item > Num ->
+	filter(Rest, Num);
+filter([Item|Rest], Num) ->
+	[Item|filter(Rest, Num)].
+%---
+reverse(List) ->
+	reverse(List, []).
+reverse([], Acc) -> Acc;
+reverse([Head|Rest], Acc) ->
+	reverse(Rest, [Head|Acc]).
+%---
+concatenate(List) ->
+	concatenate(List, []).
+
+concatenate([], Acc) -> lists:reverse(Acc);
+concatenate([Head|Tail], Acc) ->
+	case Head of 
+		[] -> concatenate(Tail, Acc);
+		_ -> concatenate(Tail, item(Head, Acc))
+	end.
+
+item([], Acc) -> Acc;
+item([El|Rest], Acc) ->
+	item(Rest, [El|Acc]).
+%---
+
