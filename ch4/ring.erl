d88b59c7ab879d592ecc0e66916357afb1a82204
boleque <vlkislitsyn1090@gmail.com>
2022-04-11 12:01:00 +0300

    Improvements for the ring task

--- ch4/ring.erl
+++ ch4/ring.erl
@@ -1,27 +1,45 @@
 -module(ring).
 -export([start/3]).
+ 
+
+loop(ChildPid) ->
+    receive
+        quit ->
+            ChildPid ! quit;
+        {message, 0, _Msg} -> 
+            io:format("Received the last message, finish message handling...~n"),
+            ChildPid ! quit;
+        {message, M, Msg} ->
+            io:format("Received the message M=~p~n", [M]),
+            ChildPid ! {message, M-1, Msg}, 
+            loop(ChildPid)
+    end.
+
 
 start(M, N, Msg) ->
-    {ok, Pid} = start_proc(N-1, self()),
-    ok = sendMessage(M, Pid, Msg),
-    Pid ! quit,
+    io:format("Start the parent process~n"),
+    ParentPid = self(),
+    ChildPid = spawn(fun() -> start_process(N-1, ParentPid) end),
     receive
-        quit -> {ok, quit}
-    end.
+        {ring_complete} -> ChildPid ! {message, M-1, Msg}, loop(ChildPid)
+    end,
+    io:format("Stop the parent process~n").
 
-start_proc(0, Pid) -> {ok, Pid};
-start_proc(N, Pid) ->
-    start_proc(N-1, spawn(fun() -> loop(Pid) end)).
 
-sendMessage(0, _, _) -> ok;
-sendMessage(M, Pid, Msg) ->
-    Pid ! {message, Msg}, 
-    receive 
-        {message, GotMsg} -> sendMessage(M-1, Pid, GotMsg)
-    end.
+start_process(0, ParentPid) ->
+    io:format("Start last child process~n"),
+    ParentPid ! {ring_complete},
+    loop(ParentPid),
+    io:format("Stop the last child process~n");
 
-loop(Pid) ->
-    receive
-        {message, _} = Msg -> Pid ! Msg, loop(Pid);
-        quit -> Pid ! quit
-    end.
+
+start_process(N, ParentPid) ->
+    io:format("Start child process=~p~n", [N]),
+    ChildPid = spawn(fun() -> start_process(N-1, ParentPid) end),
+    loop(ChildPid),
+    io:format("Stop the process with N=~p~n", [N]).
+
+
+
+
+
