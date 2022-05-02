95aff3e140654f236cd3726db8c0c4c504df54f9
boleque <vlkislitsyn1090@gmail.com>
2022-05-02 23:50:53 +0300

    Task 7-1 done

--- ch7/records.erl
+++ ch7/records.erl
@@ -1,7 +1,7 @@
 -module(records).
 -export([birthday/1, joe/0, showPerson/1]).
 
--record(person, {name,age=0,phone}).
+-record(person, {name,age=0,phone,address}).
 
 birthday(#person{age=Age} = P) ->
   P#person{age=Age+1}.
@@ -9,9 +9,10 @@ birthday(#person{age=Age} = P) ->
 joe() ->
   #person{name="Joe",
           age=21,
-          phone="999-999"}.
+          phone="999-999",
+          address="Tallinn"}.
 
-showPerson(#person{age=Age,phone=Phone,name=Name}) ->
-  io:format("name: ~p  age: ~p  phone: ~p~n", [Name,Age,Phone]).
+showPerson(#person{age=Age,phone=Phone,name=Name,address=Address}) ->
+  io:format("name: ~p  age: ~p  phone: ~p address: ~p ~n", [Name,Age,Phone,Address]).
 
 
