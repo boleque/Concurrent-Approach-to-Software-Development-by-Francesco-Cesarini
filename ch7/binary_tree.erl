77c332669b50f25b11d1fd719944f530a5762acd
boleque <vlkislitsyn1090@gmail.com>
2022-05-04 23:40:43 +0300

    Task 7-5 record, sum and max_node functions defined

--- ch7/binary_tree.erl
+++ ch7/binary_tree.erl
@@ -1,1 +1,23 @@
 -module(binary_tree).
+-export([sum/1, max_node/1]).
+-record(tree_node, {value, left_child=null, right_child=null}).
+
+
+sum(#tree_node{
+    value=Value,
+    left_child=Left,
+    right_child=Right
+} = _Root) ->
+    Value + sum(Left) + sum(Right);
+
+sum(null) ->
+    0.
+
+max_node(#tree_node{value=Value, right_child=Right} = _Root) ->
+    max_node_acc(Right, Value).
+
+max_node_acc(#tree_node{value=Value, right_child=Right} = _Node, _MaxValue) ->
+    max_node_acc(Right, Value);
+
+max_node_acc(null, Max) ->
+    Max.
