1160a457b1edfc8992945cf6adf49d46677512bc
boleque <vlkislitsyn1090@gmail.com>
2022-05-05 12:43:59 +0300

    Task 7-5 naming fixed

--- ch7/binary_tree.erl
+++ ch7/binary_tree.erl
@@ -1,6 +1,6 @@
 -module(binary_tree).
--export([sum/1, max_node/1, is_valid/1]).
--export([is_valid_helper/3]).
+-export([sum/1, max_node/1, is_binary_tree/1]).
+-export([is_bt_helper/3]).
 -record(tree_node, {value, left_child=null, right_child=null}).
 
 
@@ -24,20 +24,20 @@ max_node_acc(#tree_node{value=Value, right_child=Right} = _Node, _MaxValue) ->
 max_node_acc(null, Max) ->
     Max.
 
-is_valid(Root) ->
-    is_valid_helper(Root, -1000000, 1000000).
+is_binary_tree(Root) ->
+    is_bt_helper(Root, -1000000, 1000000).
 
-is_valid_helper(null, _Min_value, _Max_value) ->
+is_bt_helper(null, _Min_value, _Max_value) ->
     true;
 
-is_valid_helper(#tree_node{value=Value, left_child=Left, right_child=Right}=_Node, Min_value, Max_value) ->
+is_bt_helper(#tree_node{value=Value, left_child=Left, right_child=Right}=_Node, Min_value, Max_value) ->
     if
         Value < Min_value ->
             false;
         Value > Max_value ->
             false;
         true ->
-            A = is_valid_helper(Right, Value, Max_value),
-            B = is_valid_helper(Left, Min_value, Value),
+            A = is_bt_helper(Right, Value, Max_value),
+            B = is_bt_helper(Left, Min_value, Value),
             A and B
     end.
