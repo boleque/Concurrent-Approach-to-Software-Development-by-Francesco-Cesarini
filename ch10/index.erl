f2d91b4855009ef5def14202423bf58e02e78c18
boleque <vlkislitsyn1090@gmail.com>
2022-05-18 22:15:34 +0300

    Task 10-1 done

--- ch10/index.erl
+++ ch10/index.erl
@@ -3,6 +3,8 @@
 -export([index/1,processFile/1,prettyEntry/1]).
 -export([accumulate/1, accumulate/2]).
 -export([prettyList/1]).
+-export([pad/2]).
+-export([test/0]).
 
 
 index(File) ->
@@ -26,19 +28,22 @@ processLines(IoDevice,N) ->
 -define(Punctuation,"(\\ |\\,|\\.|\\;|\\:|\\t|\\n|\\(|\\))+").
 
 processLine(Line,N) ->
-  case regexp:split(Line,?Punctuation) of
-    {ok,Words} ->
-      processWords(Words,N) ;
-    _ -> []
+  case re:split(Line,?Punctuation) of
+    Words ->
+      processWords(Words,N);
+    _ ->
+    []
   end.
 
 processWords(Words,N) ->
   case Words of
     [] -> ok;
     [Word|Rest] ->
+      %io:format("Here Iam LEN:~p~n", [length(binary_to_list(Word))]),
+      WordConverted = binary_to_list(Word),
       if
-        length(Word) > 3 ->
-          Normalise = string:to_lower(Word),
+        length(WordConverted) > 3 ->
+          Normalise = string:to_lower(WordConverted),
           ets:insert(indexTable,{{Normalise , N}});
         true -> ok
       end,
@@ -73,8 +78,9 @@ prettyIndexNext(Entry,{Word, Lines}=IndexEntry) ->
       end
   end.
 
-prettyEntry(IndexEntry) ->
-  % exercise
+prettyEntry({Word, Lines}) ->
+  io:put_chars(pad(20, Word)),
+  prettyList(accumulate(Lines)),
   ok.
 
 %%
@@ -125,8 +131,52 @@ prettyList([{StartEl}|Rest]) ->
   io:format("~p,",[StartEl]),
   prettyList(Rest).
 
+%%
+pad(N,Word) ->
+  Len = length(Word),
+  if
+	  Len >= N -> Word;
+	  true -> Word ++ add_spaces(N-Len)
+  end.
 
+add_spaces(0) ->
+  [];
 
+add_spaces(N) ->
+  " " ++ add_spaces(N-1).
 
+test() ->
+    index("Text.txt").
+
+%lorem               1-2,6,8.
+%make                3.
+%more                7.
+%only                4.
+%pagemaker           7.
+%passages            6.
+%popularised         5.
+%printer             3.
+%printing            1.
+%publishing          7.
+%recently            7.
+%release             6.
+%remaining           5.
+%scrambled           3.
+%sheets              6.
+%simply              1.
+%since               2.
+%software            7.
+%specimen            4.
+%standard            2.
+%survived            4.
+%text                1-2.
+%took                3.
+%type                3.
+%typesetting         1,5.
+%unchanged           5.
+%unknown             3.
+%versions            8.
+%when                3.
+%with                6-7.
 
     
